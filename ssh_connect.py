e8f8f9d18ebc0e2d0c721a001be313ffc75069fc
Bryce Sampson <sampson.bryce@yahoo.com>
2017-02-28 21:42:58 -0800

    switching to linux for excpect

---
+++ ssh_connect.py
@@ -0,0 +1,40 @@
+import pexpect
+
+PROMPT = ['# ', '>>> ', '> ', '\$ ']
+
+
+def send_command(child, command):
+    child.sendline(command)
+    child.expect(PROMPT)
+    print(child.before)
+
+
+def connect(user, host, password):
+    ssh_newkey = "Are you sure you want to continue connecting"
+    connStr = 'ssh ' + user + '@' + host
+    child = pexpect.popen_spawn.PopenSpawn(connStr)
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
+    if ret == 0:
+        print('[-] Error Connecting')
+        return
+    if ret == 1:
+        child.sendline('yes')
+        ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
+        if ret == 0:
+            print('[-] Error Connecting')
+            return
+
+    child.sendline(password)
+    child.expect(PROMPT)
+    return child
+
+
+def main():
+    host = "192.168.1.144"
+    user = 'sampsonbryce'
+    password = 'Santacruz1'
+    child = connect(user, host, password)
+    send_command(child, 'ls ~')
+
+if __name__ == "__main__":
+    main()
